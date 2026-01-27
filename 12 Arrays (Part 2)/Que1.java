2eff75c49e6687addcbdae6658bff88a019f1a58
shubham-pattewar <sspattewar2004@gmail.com>
2026-01-27 22:18:37 +0530

    Duplicate Element

---
+++ 12 Arrays (Part 2)/Que1.java
@@ -0,0 +1,23 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, 
+// and return false if every element is distinct
+
+// Example 1: Input:nums = [1, 2, 3, 1] Output:   true 
+// Example 2: Input:nums = [1, 2, 3, 4] Output:   false 
+// Example 3: Input:nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]  Output:   true
+
+public class Que1 {
+    public static boolean distinctElement(int nums[]){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }  
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(distinctElement(nums));
+    }
+}
