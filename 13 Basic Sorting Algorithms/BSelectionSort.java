d98db01da79f8e7019b141f9a3f6957a225f3e04
shubham-pattewar <sspattewar2004@gmail.com>
2026-01-28 00:34:06 +0530

    Selection Sort Added

---
+++ 13 Basic Sorting Algorithms/BSelectionSort.java
@@ -0,0 +1,26 @@
+public class BSelectionSort {
+    public static void selectionSort(int arr[]) {
+        for(int i = 0; i < arr.length-1; i++){
+            int minPos = i;
+            for(int j = i+1 ; j < arr.length; j++){
+                if(arr[minPos] > arr[j]){           // just revert this for decreasing order
+                    minPos = j;
+                }
+            }
+            //swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        selectionSort(arr);
+        printArr(arr);
+    }
+}
