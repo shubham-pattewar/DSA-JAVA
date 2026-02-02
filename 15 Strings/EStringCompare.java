40f8ab428fe512beac56e5e724ac9779fcce9330
shubham-pattewar <sspattewar2004@gmail.com>
2026-02-02 18:43:55 +0530

    String Functions Added

---
+++ 15 Strings/EStringCompare.java
@@ -0,0 +1,25 @@
+public class EStringCompare {
+    public static void main(String[] args) {
+        String s1 = "Tony";
+        String s2 = "Tony";
+        String s3 = new String("Tony");
+
+        if(s1==s2){
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("strings are not equal");
+        }
+
+        if(s1==s3){
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("strings are not equal");
+        }
+
+        if(s1.equals(s3)){
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("strings are not equal");
+        }
+    }
+}
---
+++ 15 Strings/FStringSubstring.java
@@ -0,0 +1,18 @@
+public class FStringSubstring {
+    public static String subString(String str, int si, int ei){
+        String substr = "";
+        for(int i = si; i < ei; i++){
+            substr += str.charAt(i);
+        }
+        return substr;
+    }
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        System.out.println(subString(str, 0, 4));
+        System.out.println(subString(str, 3, 5));
+
+        System.out.println(str.substring(0,5));
+
+    }
+}
